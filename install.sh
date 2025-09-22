1448c1f4a8bdbe68ae739b848949e0fceccaa783
Daniel彡 <jose.saldanafrs@uanl.edu.mx>
2025-09-21 20:14:22 -0500

    Modify install script

--- install.sh
+++ install.sh
@@ -10,19 +10,17 @@ echo "Dosan's Minecraft Hosting Script!"
 
 # Variables
 MC_DIR="$HOME/mc-spigot"
-DOWNLOAD_URL="https://hub.spigotmc.org/jenkins/job/BuildTools/195/artifact/target/BuildTools.jar"
+$DOWNLOAD_URL="https://api.papermc.io/v2/projects/paper/versions/1.21.1/builds/130/downloads/paper-1.21.1-130.jar" 
 
 # Create server directory
 mkdir -p "$MC_DIR" # -p create automatically without raising an error
 cd "$MC_DIR" || exit
 
 # Download the server if not already present
-wget "$DOWNLOAD_URL" -O "bedrock-server-$MC_VERSION.zip"
+wget "$DOWNLOAD_URL" -O "paper.jar"
 
-java -jar BuildTools.jar --rev 1.20.1
-
-# Accept EULA
 echo "eula=true" > eula.txt
 
-java -Xms1G -Xmx2G -jar spigot-1.20.1.jar nogui
 echo "Server started. You can now connect to your Minecraft server!"
+
+java -Xms1G -Xmx2G -jar paper.jar nogui
